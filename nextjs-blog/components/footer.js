882d74d04ad06da1a03a7e38ee3b7441dace06e7
hzethraeus <hzethraeus@gmail.com>
2022-04-08 17:17:52 +0200

    Component fix

--- nextjs-blog/components/footer.js
+++ nextjs-blog/components/footer.js
@@ -5,12 +5,20 @@ export default function Footer() {
 
   return (
     <footer className={styles.footer}>
-        <div>
+        <div className={styles.lowDiv}>
             <h5>Szabina</h5>
             <h6>
                 Adress <br/>
                 Number <br/>
-                Hejsan
+                Email
+            </h6>
+        </div>
+        <div className={styles.lowDiv}>
+            <h5>Social Media</h5>
+            <h6>
+            <a href="https://www.instagram.com/szabina.locki/" target="_blank" >Instagram</a> <br/>
+                Facebook <br/>
+                Hej
             </h6>
         </div>
         
