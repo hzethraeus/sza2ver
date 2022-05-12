314143f570dcf5aa9712a4130f795ca79d3cd2e9
hzethraeus <hzethraeus@gmail.com>
2022-05-12 11:53:06 +0200

    header & portfolio

--- nextjs-blog/components/header.js
+++ nextjs-blog/components/header.js
@@ -7,43 +7,7 @@ import {useEffect, useState} from 'react';
 
 export default function Header() {
     const router=useRouter();
-    /*
-    var imgSrc="/images/j_logo.jpeg";
-    var x ="" 
-    var imgHeight=50;
-    var imgWidth=50;
-    if(typeof window !== 'undefined') {
-        // detect window screen width function
-        x=window.matchMedia("(max-width: 500px)")
-        if (x.matches) { // If media query matches
-            imgHeight=50;
-           imgWidth=50;
-           imgSrc="/images/j_logo_small.png";
-           
-          } else {
-            imgHeight=108;
-            imgWidth=200;
-            imgSrc="/images/j_logo.jpeg"; // Route of the image file
-           
-          }
-      }
-    
-    useEffect(()=>{
-        if (x.matches) { // If media query matches
-            imgHeight=50;
-           imgWidth=50;
-           imgSrc="/images/j_logo_small.png";
-           
-          } else {
-            imgHeight=108;
-            imgWidth=200;
-            imgSrc="/images/j_logo.jpeg"; // Route of the image file
-            
-          }
-    }, [x]);
- 
-*/
-    //console.log(router.pathname.startsWith("/about/about"))
+  
   return (
     <header>
         <div className={styles.headerAll}>
